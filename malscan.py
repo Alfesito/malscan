8d93e92f170a5c0d17fb8f5cc9f054d08222454a
Andrés Alfaro Fernández <andresalfe@gmail.com>
2023-09-28 00:39:55 +0200

    vt con todo

--- malscan.py
+++ malscan.py
@@ -1,20 +1,20 @@
-import argparse
 import hashlib
 import os
+import sys
 import json
 import requests
 from virus_total_apis import PublicApi as VirusTotalPublicApi
 from colorama import Fore, Back, Style, init
 
 # Tu clave de API de VirusTotal
-API_KEY = 'your_api_key'
+API_KEY_VT = '4e603c3a01a10515c1b1da28ccf061540958ed1b7932867e458774a4771c785b'
 URL_SERCH_VT = 'https://www.virustotal.com/api/v3/search'
 
 # Calcula el hash md5 para un archivo
-def calcular_md5(file_path):
+def calcular_md5(path):
     try:
         # Abre el archivo en modo binario
-        with open(file_path, 'rb') as archivo:
+        with open(path, 'rb') as archivo:
             # Lee el contenido del archivo en bloques de 4096 bytes
             md5 = hashlib.md5()
             while True:
@@ -26,14 +26,18 @@ def calcular_md5(file_path):
     except FileNotFoundError:
         return None
 
-def analizar_archivo(api_key, file_path):
+# VirusTotal api: https://developers.virustotal.com/reference/overview
+def analizar_vt(API_KEY_VT, path, isfile):
+    if argisfile:
+        query = calcular_md5(path)
+    else:
+        query = path
 
-    hash = calcular_md5(file_path)
     headers = {
         "accept": "application/json",
-        "x-apikey": api_key
+        "x-apikey": API_KEY_VT
     }
-    response = requests.get(URL_SERCH_VT+'?query='+str(hash), headers=headers)
+    response = requests.get(URL_SERCH_VT+'?query='+str(query), headers=headers)
 
     if response.status_code == 200:
         # Obtener las categorías de análisis
@@ -42,21 +46,30 @@ def analizar_archivo(api_key, file_path):
         reputation = data['data'][0]['attributes']['reputation']           
 
         # Comprobar si al menos una categoría es "malicious"
-        if analysis['malicious'] > 0 or reputation < 1:
-            print(Fore.RED + "\nNo es seguro instalar el ejecutable segun VirusTotal.")
+        if analysis['malicious'] > 0 or (reputation <= 0 and isfile):
+            print(Fore.RED + "\n[-] NO es seguro segun VirusTotal.")
         else:
-            print("\nEl ejecutable es seguro segun VirusTotal.")
+            print(Fore.GREEN + "\n[+] Es seguro segun VirusTotal.")
     else:
-        print(Fore.GREEN + Fore.YELLOW + "\nError al buscar en VirusTotal.")
+        print(Fore.YELLOW + "\nError al buscar en VirusTotal.")
+
+# Aanalizando con https://www.filescan.io
+def analizar_fs(url):
+    print('fs')
 
 if __name__ == "__main__":
     # Inicializa colorama
     init(autoreset=True)
-    parser = argparse.ArgumentParser(description="Analizar un archivo con VirusTotal")
-    parser.add_argument("archivo", help="Ruta al archivo ejecutable a analizar")
-    args = parser.parse_args()
 
-    if not os.path.exists(args.archivo):
-        print(Fore.YELLOW + "\nEl archivo "+args.archivo+" no existe.")
+    argisfile = True
+    args = sys.argv[1]
+
+    if not os.path.exists(args):
+        if args:
+            argisfile = False
+            analizar_vt(API_KEY_VT, args, argisfile)
+            analizar_fs(args)
+        else:
+            print(Fore.YELLOW + "\nEl parámetro es incorrecto.")
     else:
-        analizar_archivo(API_KEY, args.archivo)
+        analizar_vt(API_KEY_VT, args, argisfile)
